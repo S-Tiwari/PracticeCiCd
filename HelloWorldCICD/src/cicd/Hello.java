38c2d898f0b18c9aba5262d3f2c90e8ba629f880
ShwetaT <shweta.tiwari@nice.com>
2017-05-30 00:12:26 +0530

    first project in public repo

---
+++ HelloWorldCICD/src/cicd/Hello.java
@@ -0,0 +1,10 @@
+package cicd;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ HelloWorldCICD/src/cicd/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author shtiwari
+ *
+ */
+package cicd;
